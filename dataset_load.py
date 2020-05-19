902dd3405f869a4788305ebfebc424647b36b4a2
Bhavya Vasudeva <30321205+estija@users.noreply.github.com>
2020-05-19 17:20:36 +0530

    Update dataset_load.py

--- dataset_load.py
+++ dataset_load.py
@@ -27,15 +27,31 @@ def load_a(path, num):
         data = np.asarray(data)
         return data
 
+def load_b(path):
+    f = os.listdir(path)
+    data = []
+    #use imgs with more than 10% non-zero values
+    n_zero_ratio = 0.1
+    for i in range(len(f)):
+      img = os.path.join(path, f[i])
+      data_new=np.load(img, allow_pickle =True )
+      data_new=data_new.astype('float32')
+      for j in range(data_new.shape[0]): 
+        if (float(np.count_nonzero(data_new[j,:,:]))/np.prod(data_new[j,:,:].shape))>=n_zero_ratio:
+          data_new[j,:,:] = data_new[j,:,:]/127.5-1.0    
+          data.append(data_new[j,:,:])
+    data = np.asarray(data)
+    return data
+
 def train_data_aug(train_gt):
+    #for mrnet, replace the indices with the commented ones
+    gt1=train_gt[0:13461,:,:]       #0:8100
 
-    gt1=train_gt[0:13461,:,:]
+    gt2a=train_gt[12471:12966,:,:]  #7500:7800    #overlapping data
+    gt2b=train_gt[12966:13461,:,:]  #7800:8100    #overlapping data
 
-    gt2a=train_gt[12471:12966,:,:]#overlapping data
-    gt2b=train_gt[12966:13461,:,:]#overlapping data
-
-    gt3a=train_gt[13461:16629,:,:]#non-overlapping data
-    gt3b=train_gt[16629:19797,:,:]#non-overlapping data
+    gt3a=train_gt[13461:16629,:,:]  #8100:10000   #non-overlapping data
+    gt3b=train_gt[16629:19797,:,:]  #10000:11900  #non-overlapping data
 
     gt2=np.vstack((gt2a,gt2b))
     gt3=np.vstack((gt3a,gt3b))
@@ -46,8 +62,14 @@ def train_data_aug(train_gt):
     return gt_new
 
 #for training data
+
+#miccai dataset
 train_path='/home/cs-mri-gan/training-training/warped-images'
 train_gt=load_a(train_path,1090)
+
+#mrnet dataset
+#train_path='/home/cs-mri-gan/train/coronal'
+#train_gt=load_b(train_path)
 
 train_gt_aug=train_data_aug(train_gt) #created gt for augmented data
 
@@ -56,8 +78,15 @@ with open(os.path.join(save_path,'training_gt_aug.pickle'),'wb') as f:
 
 '''
 #for testing data
+
+#miccai dataset
 test_path='/home/cs-mri-gan/training-testing/warped-images'
 test_data=load_a(test_path, 390)
+
+#mrnet dataset
+#test_path='/home/cs-mri-gan/valid/coronal'
+#test_data=load_b(test_path)
+
 with open(os.path.join(save_path,'testing_gt.pickle'),'wb') as f:
        pickle.dump(test_data,f,protocol=4)
 '''
